03698a8afcc450cb7dd1177fedbd86cca0ae04c8
Ivan <pig1995z@gmailcom>
2020-09-06 18:44:48 +0300

    Updates

---
+++ precache-manifest.c186d7ce63c607c21200077837b3f3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589bed22be328cd1c069da2afb22d483",
+    "url": "/hw1react/index.html"
+  },
+  {
+    "revision": "02dc451467248ab0ff23",
+    "url": "/hw1react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab3786f5714ecda51e29",
+    "url": "/hw1react/static/js/2.608df4b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw1react/static/js/2.608df4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dc451467248ab0ff23",
+    "url": "/hw1react/static/js/main.dd3712af.chunk.js"
+  },
+  {
+    "revision": "d4b4ad3bc7c53a1292cc",
+    "url": "/hw1react/static/js/runtime-main.af6dc9d1.js"
+  }
+]);
