25e07c25d36dbf95178d596fc8cef509e03073ff
Ivan <pig1995z@gmailcom>
2020-09-13 22:15:06 +0300

    add build

---
+++ build/precache-manifest.3bb73cd02924fedd24d6ba1d5a954062.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1875a077eb83a353b5851bdcc65641",
+    "url": "/hw1react/index.html"
+  },
+  {
+    "revision": "05c1e61634adc7f4adf7",
+    "url": "/hw1react/static/css/main.4a094cc1.chunk.css"
+  },
+  {
+    "revision": "b09934291588aa7f2ddf",
+    "url": "/hw1react/static/js/2.6e7475a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw1react/static/js/2.6e7475a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c1e61634adc7f4adf7",
+    "url": "/hw1react/static/js/main.0d297644.chunk.js"
+  },
+  {
+    "revision": "d4b4ad3bc7c53a1292cc",
+    "url": "/hw1react/static/js/runtime-main.af6dc9d1.js"
+  },
+  {
+    "revision": "21bafa00cfb431370166fa5a1b06db35",
+    "url": "/hw1react/static/media/EhN8T5hWoAYlzOx.21bafa00.jpg"
+  },
+  {
+    "revision": "0d3e1a7392657609ee27625a3483d709",
+    "url": "/hw1react/static/media/F2Aws-10.0d3e1a73.jpg"
+  },
+  {
+    "revision": "8ae11037b7fce02a7f96ee5fa3056652",
+    "url": "/hw1react/static/media/belarus.8ae11037.jpg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/hw1react/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/hw1react/static/media/heart1.a8cd76c1.svg"
+  },
+  {
+    "revision": "f5840484ce92b9ad8f10fbfb051ea61d",
+    "url": "/hw1react/static/media/q_0-Yddo.f5840484.jpg"
+  }
+]);
