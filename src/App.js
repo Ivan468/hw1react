fcb1f018b0e260106696fe8fa9b117c1082e5bd3
Ivan <pig1995z@gmailcom>
2020-09-16 09:54:30 +0300

    hw 4 react material design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AppSear from "./components/searchfilter";
 import { userList } from "./components/info-json";
 // import TimerTwo from "./components/timer_two";
 import TimerHook from "./components/timerHook";
+import Login from "./components/login/login";
+import SignUp from "./components/login/signUp";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
       <TimerHook time="1000" autostart="true" int="1" name="першому таймеру"/>
       <TimerHook time="100" autostart="false" int="3" name="другому таймеру" /> 
       <TimerHook time="120" autostart="false" int="3" name="Третьому таймеру" /> 
+      <Login/>
+      <SignUp/>
     </div>
   );
 }
