53dcdd1f9024776535c7501562a4890d21244f97
Ivan <pig1995z@gmailcom>
2020-09-16 23:58:11 +0300

    router

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ import { userList } from "./components/info-json";
 import TimerHook from "./components/timerHook";
 import SignIn from "./components/login/login";
 import SignUp from "./components/login/signUp";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
+     <Router>
       {userList.map((user) => (
         <Users {...user} />
       ))}
@@ -24,8 +31,19 @@ function App() {
       <TimerHook time="1000" autostart="true" int="1" name="першому таймеру"/>
       <TimerHook time="100" autostart="false" int="3" name="другому таймеру" /> 
       <TimerHook time="120" autostart="false" int="3" name="Третьому таймеру" /> 
-      <SignIn/>
-      <SignUp/>
+
+      
+     
+
+      <Switch>
+          <Route  path="/signUp">
+          <SignUp/>
+          </Route>
+          <Route path="/">
+          <SignIn/> 
+          </Route>
+        </Switch>
+        </Router>
     </div>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import LinkR from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -13,6 +13,12 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {  ThemeProvider, createMuiTheme  } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function Copyright() {
     return (
@@ -117,7 +123,7 @@ function Copyright() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="#" variant="body2" to="/signUp">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
