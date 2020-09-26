2077a7dfa3b28fab6461b68bcb433ef2b4d2248f
Ivan <pig1995z@gmailcom>
2020-09-26 18:32:15 +0300

    add compose devtools

--- src/tweets/store.js
+++ src/tweets/store.js
@@ -1,10 +1,15 @@
 //we have decided that we are using redux so we have to install and  import it
-import {createStore} from 'redux';
+import { createStore, compose } from "redux";
 //we need our data to put in the database
 import state from "./state";
 import reducers from "./redusers/index";
 
-var storeTwo = createStore(reducers, state);
-
+var storeTwo = createStore(
+  reducers,
+  state,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default storeTwo;
